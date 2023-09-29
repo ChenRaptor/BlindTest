044556a99fa8aa087a6d7b3f5587b7bea3b07cc0
Chen <ChenRaptor@outlook.fr>
2023-09-29 13:58:10 +0200

    base2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { SocketProvider } from '@/providers/socket-provider'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
@@ -16,7 +17,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <SocketProvider>
+        {children}
+      </SocketProvider>
+      </body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,21 @@
+"use client"
 import Image from 'next/image'
 import styles from './page.module.scss'
 import SocketQRCode from '@components/QrCode/QrCode'
+import { useSocket } from '@/providers/socket-provider'
+import { useEffect } from "react"
 
 export default function Home() {
+
+  const {socket, isConnected} = useSocket()
+
+  useEffect(() => {
+    console.log(socket);
+    if (socket) {
+      socket.emit("connect")
+    }
+  },[socket])
+
   return (
     <main className={styles.main}>
       <SocketQRCode/>
