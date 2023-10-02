7ed33aff22b86e06cbed5646ea4a79d39a14b706
Chen <ChenRaptor@outlook.fr>
2023-10-02 14:38:32 +0200

    phone-and-tablet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
 "use client"
-import { useState } from 'react'
 import styles from './page.module.scss'
-import Sound from '@components/Sound/Sound'
-import UserList from '@components/UserList/UserList'
-import ScoreBoard from '@components/ScoreBoard/ScoreBoard'
-import Answer from '@components/Answer/Answer'
 
 import SocketQRCode from '@components/QrCode/QrCode'
 import Link from 'next/link';
@@ -12,20 +7,20 @@ import { v4 as uuidv4 } from 'uuid';
 
 export default function Home() {
 
-  const socketURL = `${process.env.NEXT_PUBLIC_SITE_URL}/room/${uuidv4()}`;
-  const [user, setUser] = useState<string[]>(['Jean','Marie','Rose','Albert','Hugo'])
+  const socketURL = `${process.env.NEXT_PUBLIC_SITE_URL}/room/${uuidv4()}/phone`;
+  // const [user, setUser] = useState<string[]>(['Jean','Marie','Rose','Albert','Hugo'])
 
 
   return (
     <main className={styles.main}>
-      {/* <div>
-        <ScoreBoard game={user}/>
-        <UserList list={user}/>
-      </div>
-      <Sound/>
-      <Answer/> */}
+
+
+
       <Link href={socketURL}>Lien vers la room</Link>
       <SocketQRCode url={socketURL}/>
+
+
+
     </main>
   )
 }
